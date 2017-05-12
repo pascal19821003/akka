f5a468025b8255f57b552a26f4040a3f38eb4391
Satendra Tiwari <sattiwari18@gmail.com>
2017-05-12 12:08:28 +0700

    find actor ref using path

--- src/main/scala/actor/HelloWorldDemo.scala
+++ src/main/scala/actor/HelloWorldDemo.scala
@@ -1,5 +1,6 @@
 package actor
 
+import actor.AkkaAskDemo.res
 import actor.HelloWorldDemo.{a, system}
 import akka.actor._
 import akka.pattern.ask
@@ -8,6 +9,7 @@ import scala.concurrent.Await
 import akka.util.Timeout
 
 import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class HelloActor extends Actor {
   def receive: Receive = {
@@ -36,11 +38,28 @@ object HelloWorldDemo extends App {
 
 object AkkaAskDemo extends App {
   val system = ActorSystem()
-  val a = system.actorOf(Props[HelloActor])
+  val a = system.actorOf(Props[HelloActor], "hello")
   system.actorOf(Props(classOf[Terminator], a))
   implicit val timeout = Timeout(5 seconds)
   val res = a ? "hi-ask"
   val x = Await.result(res, 2 seconds)
   println(x)
+  system.stop(a)
+}
+
+object ActorPathDemo extends App {
+  val system = ActorSystem()
+  val a = system.actorOf(Props[HelloActor])
+  system.actorOf(Props(classOf[Terminator], a))
+
+  implicit val timeout = Timeout(5 seconds)
+  val f = (system.actorSelection(a.path) ? Identify(None))
+
+  f.onSuccess {
+    case ActorIdentity(_, Some(ref)) =>
+      ref ! "hi"
+    case _ => println("cant get actor reference")
+  }
+
   system.stop(a)
 }
